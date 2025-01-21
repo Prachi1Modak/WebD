55d2091e2c421d66b98b7b6d788c7a15f9de3e9c
Prachi1Modak <123183519+Prachi1Modak@users.noreply.github.com>
2025-01-21 14:11:00 +0000

    learnt datatypes

---
+++ js_basics/datatypes.js
@@ -0,0 +1,31 @@
+"use strict"    //this basically operates your code according to the newer js version, not the old ones
+
+// alert("Prachi")
+
+/*this alert written here, would give, alert not defined, but if done in browser console directly, gives a popup!
+ because we are using nodejs */
+
+ let name="prachi"
+ let age=3
+ console.log(name)
+ console.log(age)
+
+ /*
+ number=2^53
+ bigint
+ string ""
+ boolean
+null->empty*
+undefined ->not yet defined
+symbol -> used for uniquness
+
+OBJECT
+ */
+
+console.log(typeof "abc")
+console.log(typeof name)
+console.log(typeof age)
+
+console.log(typeof null)        //object
+
+console.log(typeof undefined)       //undefined
